25cc6491ebe15e2612203575db5cbcec758ac7de
Joon Park <JoonPark2017@u.northwestern.edu>
2017-02-19 22:49:27 -0600

    Load data into mongodb
    load_data, when run, loads the relevant nuetron star data from the text
    files into a mongodb db called neutron_stars. The script currently only
    loads single neutron stars.

--- load_script.py
+++ load_script.py
@@ -1,4 +1,6 @@
 import os
+import glob
+from pymongo import MongoClient
 
 NEUTRON_STAR = 13
 
@@ -41,8 +43,11 @@ def parse_sev(sev_name):
     # This should only be required in case there is no -1000 EOF value
     return { "time": physical_time, "stars": stars }
 
-def parse_save(save_dir):
-    """Takes in a save directory address and returns a list of stars.
+def parse_save(save_dir, neutron_only = True):
+    """Takes in a save directory address and returns a list of neutron stars.
+
+    If neutron_only is False, return all stars
+    data should be sorted by 'time'
 
     output format: [
         {
@@ -56,7 +61,79 @@ def parse_save(save_dir):
         }, ...
     ]
     """
-    pass
+    files = os.path.join(save_dir, 'sev.83_*')
+
+    stars = []
+    for filename in glob.glob(files):
+        sev_data = parse_sev(filename)
+        for star in sev_data['stars']:
+            if (neutron_only and star['type'] == NEUTRON_STAR) or (not neutron_only):
+                matching_star = filter(lambda x: x['id'] == star['id'], stars)
+                if matching_star:
+                    matching_star[0]['data'].append({
+                        'time': sev_data['time'],
+                        'type': star['type']
+                    })
+                else:
+                    stars.append({
+                        'id': star['id'],
+                        'data': [{
+                            'time': sev_data['time'],
+                            'type': star['type']
+                        }]
+                    })
+    # Sort
+    for star in stars:
+        star['data'] = sorted(star['data'], key=lambda x: x['time'])
+
+    return stars
+
+def parse_run(run_dir, neutron_only = True):
+    """Given a run directory address, returns a list of stars
+
+    output format: [
+        {
+            'id': int,
+            'data': [
+                {
+                    'time': float,
+                    'type': int,
+                }
+            ]
+        }, ...
+    ]
+    """
+    files = os.path.join(run_dir, 'save0*')
+
+    stars = []
+    for save_dir in glob.glob(files):
+        data = parse_save(save_dir, neutron_only)
+        if len(stars) == 0:
+            stars = data
+        else:
+            # Check for overlap
+            for star in data:
+                existing = filter(lambda x: x['id'] == star['id'], stars)[0]
+                if not existing:
+                    stars.append(star)
+                else:
+                    old = existing['data']
+                    new = star['data']
+                    start_time = min([x['time'] for x in new])
+                    for i in range(len(old)):
+                        # Delete overlap if exists
+                        if old[i]['time'] >= start_time:
+                            old = old[:i]
+                            break
+                    existing['data'] = old + new
+    return stars
+
+def load_neutron_stars(run_dir, run_name):
+    """Given a run directory address, loads the run data into mongoDB"""
+    client = MongoClient()
+    db = client.neutron_stars
+    collection = db[run_name]
+    collection.insert_many(parse_run(run_dir))
 
 def main():
     params = {
@@ -64,19 +141,15 @@ def main():
         'rad': [26],
         'metallicity': ['02', '002']
     }
-
-    data_dir = os.path.join(
-        'data',
-        # "N{0}K_r{1}_Z{2}_*".format(
-        'N{0}K_r{1}_Z{2}_1'.format(
-            params['star_num'][0],
-            params['rad'][0],
-            params['metallicity'][0]
-        )
+    dir_name = 'N{0}K_r{1}_Z{2}_1'.format(
+        params['star_num'][0],
+        params['rad'][0],
+        params['metallicity'][1]
     )
 
-    sev = os.path.join(data_dir, 'save01', 'sev.83_0.000')
-    print parse_sev(sev)
+    data_dir = os.path.join('data', dir_name)
+
+    load_neutron_stars(data_dir, dir_name)
 
 if __name__== "__main__":
     main()
